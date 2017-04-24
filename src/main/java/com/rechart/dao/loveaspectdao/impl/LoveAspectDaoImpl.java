f5cb38fbf218cf333110136dedfd171750a3574c
zhougit <594389970@qq.com>
2017-04-24 21:03:22 +0800

    修复类型转换错误

--- src/main/java/com/rechart/dao/loveaspectdao/impl/LoveAspectDaoImpl.java
+++ src/main/java/com/rechart/dao/loveaspectdao/impl/LoveAspectDaoImpl.java
@@ -76,15 +76,15 @@ public class LoveAspectDaoImpl extends DaoSupport<LoveAspect> implements LoveAsp
 	public List<Double> findThisWeekRealScore(final String userId) {
 		// TODO Auto-generated method stub
 		final String sql = "select sum(round(testdifficulty/totalTimes,2))realScore from t_usertest where totalTimes > 0 and userid = ? and DATE_FORMAT(startTime,'%Y%u')=yearweek( now(),1 ) group by testaspect";
-		List listThisWeekRealScore = getHt().execute(new HibernateCallback() {
+		List<Double> listThisWeekRealScore = getHt().execute(new HibernateCallback<List<Double>>() {
 			@Override
-			public Object doInHibernate(Session session) throws HibernateException, SQLException {
+			public List<Double> doInHibernate(Session session) throws HibernateException, SQLException {
 				// TODO Auto-generated method stub
 				Connection con = session.connection();
 				PreparedStatement ps = con.prepareStatement(sql);  
 				ps.setString(1, userId);
 				ResultSet rs = ps.executeQuery();
-				List all = new ArrayList();
+				List<Double> all = new ArrayList();
 				while(rs.next()){
 					double result = rs.getDouble("realScore");
 					all.add(result); 
